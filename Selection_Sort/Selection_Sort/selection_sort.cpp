c28c27f2914ad1bc4b3b9bcfe4cab20a3bbd8f02
EoJin-Kim <linexy@naver.com>
2020-12-23 22:20:52 +0900

    test

--- Selection_Sort/Selection_Sort/selection_sort.cpp
+++ Selection_Sort/Selection_Sort/selection_sort.cpp
@@ -1,1 +1,29 @@
 #include<cstdio>
+
+int main(int argc, char* argv[]) 
+{
+	int array[10] = { 5,6,2,3,1,9,4,8,7,10 };
+	int tmp,index, min = 9999;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = i; j < 10; j++)
+		{
+			if (min > array[j])
+			{
+				index = j;
+				min = array[j];
+			}
+		}
+
+		tmp = array[i];
+		array[i] = array[index];
+		array[index] = tmp;
+		min = 9999;
+		
+
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", array[i]);
+	}
+}
